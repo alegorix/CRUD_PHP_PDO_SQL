06642a4d3bf14f1bfad83b6350c654aa7f6d09d4
alegorix <51210510+alegorix@users.noreply.github.com>
2022-02-20 00:46:32 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 try{
     // Connexion à la bdd
-    $db = new PDO('mysql:host=localhost;dbname=crud', 'root','');
+    $db = new PDO('mysql:host=localhost;dbname=crud', 'root','root');
     $db->exec('SET NAMES "UTF8"');
 } catch (PDOException $e){
     echo 'Erreur : '. $e->getMessage();
